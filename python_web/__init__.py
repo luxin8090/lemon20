09a6d60f57d361ad868f9d64ef59e71a3a7e22be
luxin8090 <313098754@qq.com>
2021-04-21 14:43:26 +0800

    1

--- python_web/__init__.py
+++
@@ -1,5 +0,0 @@
-# @Time    : 2021/4/20 0020 23:43
-# @Author  : 夏末
-# @FILE    : python_test.py
-# @QQ      : 313098754
-# @Company ：个人
--- test_data/test_data.py
+++
@@ -1,8 +0,0 @@
-# @Time    : 2021/4/20 0020 23:11
-# @Author  : 夏末
-# @FILE    : python_test.py
-# @QQ      : 313098754
-# 数据以字典格式存放
-url = {"url":'http://erp.lemfix.com/login.html'}
-user_info ={"username":"13916686542","password":"lemon123"}
-s_key = {"s_key":"806"}
